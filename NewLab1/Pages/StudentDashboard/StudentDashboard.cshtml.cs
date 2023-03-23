48692a49e92946b719aa64613796aa6c554ef43e
slashack <funkcharles@hotmail.com>
2023-03-23 10:46:39 -0400

    Add student dashboard template

---
+++ NewLab1/Pages/StudentDashboard/StudentDashboard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NewLab1.Pages.StudentDashboard
+{
+    public class StudentDashboardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
