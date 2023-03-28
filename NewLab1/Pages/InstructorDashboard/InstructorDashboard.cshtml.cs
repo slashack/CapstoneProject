1c1195ff99186a1eb69c3f6a57e86a57926ad4c2
slashack <funkcharles@hotmail.com>
2023-03-27 20:29:04 -0400

    Add initial dashboard page for student and instructor

---
+++ NewLab1/Pages/InstructorDashboard/InstructorDashboard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NewLab1.Pages.InstructorDashboard
+{
+    public class InstructorDashboardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
